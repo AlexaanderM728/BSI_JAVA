2801f008f9f480c0c74b9cbe3432c4b93e701813
ZuZa1235012350 <80256178+ZuZa1235012350@users.noreply.github.com>
2021-11-17 00:12:02 +0100

    Update CalculatorTests.java

--- Calculator_Tests_InJava/src/test/java/CalculatorTests.java
+++ Calculator_Tests_InJava/src/test/java/CalculatorTests.java
@@ -5,15 +5,15 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTests {
-    Calculator calculator;
+ Calculator calculator;
     @BeforeEach
     void setUp() {
+
         calculator = new Calculator();
     }
     @Test
     void method_calculate_01_03_should_return_99_precntage(){
         assertEquals(99.7503124074893,calculator.calcuate_01_03(0.000001,2500));
-        //TODO ogranij czy t jest spoko
     }
     @Test
     void method_calculate_01_04_should_return_0_04() {
@@ -27,15 +27,14 @@ public class CalculatorTests {
 
     @Test
     void method_calculate_01_06_should_return_0_30() {
-        assertEquals(0.6065308637049162,calculator.calculate_01_06(1000, 500));
+    assertEquals(0.6065308637049162,calculator.calculate_01_06(1000, 500));
     }
 
 
     @Test
     void method_calculate_03_01_should_return_two_given_values() {
-        //TODO zrob podobnie jak wyzej
+        assertEquals(0.6065308637049162,calculator.calculate_03_01(3,26,200, 257));
     }
-
 
 
 }
